52120476f2bc21de89bddcfb300998118f9e40b9
JAN_ADRIAN_SOSA_CORDERO <a.22308051281317@cbtis128.edu.mx>
2025-03-13 18:18:53 +0000

    examen3

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,102 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(MyApp());
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class Sucursal {
+  final String idSucursal;
+  final int numEmpleados;
+  final String telefono;
+  final String direccion;
+  final String productosOfrece;
+  final int numProductos;
+
+  Sucursal({
+    required this.idSucursal,
+    required this.numEmpleados,
+    required this.telefono,
+    required this.direccion,
+    required this.productosOfrece,
+    required this.numProductos,
+  });
+}
+
+class MyApp extends StatelessWidget {
+  final List<Map<String, dynamic>> sucursales = [
+    {
+      'id_sucursal': 'S001',
+      'num_empleados': 10,
+      'tel': '555-1234',
+      'direccion': 'Calle Falsa 123',
+      'productos_ofrece': 'Balones',
+      'num_productos': 50,
+    },
+    {
+      'id_sucursal': 'S002',
+      'num_empleados': 8,
+      'tel': '555-5678',
+      'direccion': 'Avenida Siempre Viva 456',
+      'productos_ofrece': 'Balones',
+      'num_productos': 30,
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    List<Sucursal> listaSucursales = sucursales
+        .map((map) => Sucursal(
+              idSucursal: map['id_sucursal'],
+              numEmpleados: map['num_empleados'],
+              telefono: map['tel'],
+              direccion: map['direccion'],
+              productosOfrece: map['productos_ofrece'],
+              numProductos: map['num_productos'],
+            ))
+        .toList();
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('JAN SOSA 1317'),
+        ),
+        body: Column(
+          children: listaSucursales
+              .map((sucursal) => Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: listaSucursales.indexOf(sucursal) % 2 == 0
+                            ? Colors.blue[100]
+                            : Colors.green[100],
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text('ID: ${sucursal.idSucursal}',
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold)),
+                          Text('Empleados: ${sucursal.numEmpleados}'),
+                          Text('Teléfono: ${sucursal.telefono}'),
+                          Text('Dirección: ${sucursal.direccion}'),
+                          Text('Productos: ${sucursal.productosOfrece}'),
+                          Text('Cantidad: ${sucursal.numProductos}'),
+                        ],
+                      ),
+                    ),
+                  ))
+              .toList(),
         ),
       ),
     );
